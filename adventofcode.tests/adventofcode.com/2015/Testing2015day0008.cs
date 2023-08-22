ffcc603630a14a5b7ff197952463b337cc3e84bb
Cristian Bidea <cristian.bidea@uipath.com>
2023-08-22 13:09:19 +0300

    Solved day 8

--- adventofcode.tests/adventofcode.com/2015/Testing2015day0008.cs
+++ adventofcode.tests/adventofcode.com/2015/Testing2015day0008.cs
@@ -1,12 +1,36 @@
 
+using System.Reflection;
+using adventofcode.adventofcode.com._2015;
+
 namespace adventofcode.tests.adventofcode.com._2015;
 
 public class Testing2015day0008
 {
     [TestCase]
-    public void TestSolution()
+    public void TestSolutionPart1()
     {
+        var lines = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "adventofcode.com/2015/Assets/Testing2015day0008Input.txt"));
+        Solution2015day0008.SolvePart1(lines).Should().Be(1333);
+    }
+    
+    [TestCase]
+    public void TestSolutionPart1OnTestInput()
+    {
+        var lines = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "adventofcode.com/2015/Assets/Testing2015day0008TestInput.txt"));
+        Solution2015day0008.SolvePart1(lines).Should().Be(12);
     }
 
-    private const string MainInput = @"{input}";
+    [TestCase]
+    public void TestSolutionPart2()
+    {
+        var lines = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "adventofcode.com/2015/Assets/Testing2015day0008Input.txt"));
+        Solution2015day0008.SolvePart2(lines).Should().Be(2046);
+    }
+
+    [TestCase]
+    public void TestSolutionPart2OnTestInput()
+    {
+        var lines = File.ReadAllLines(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "adventofcode.com/2015/Assets/Testing2015day0008TestInput.txt"));
+        Solution2015day0008.SolvePart2(lines).Should().Be(19);
+    }
 }
