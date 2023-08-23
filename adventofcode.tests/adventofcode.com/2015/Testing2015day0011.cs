9b52dfcf8bea5ed7087452946338a3001d41d58c
Cristian Bidea <cristian.bidea@uipath.com>
2023-08-23 22:49:18 +0300

    First try

--- adventofcode.tests/adventofcode.com/2015/Testing2015day0011.cs
+++ adventofcode.tests/adventofcode.com/2015/Testing2015day0011.cs
@@ -1,12 +1,26 @@
 
+using adventofcode.adventofcode.com._2015;
+
 namespace adventofcode.tests.adventofcode.com._2015;
 
 public class Testing2015day0011
 {
-    [TestCase]
-    public void TestSolution()
+    [TestCase("hepxcrrq", "heqqaabc")]
+    public void TestSolutionPart1(string password, string expectedNextPassword)
     {
+        var nextPassword = Solution2015day0011.SolvePart1(password.ToArray());
+        (new string(nextPassword)).Should().Be(expectedNextPassword);
     }
 
+    [TestCase("hijklmmn", false)]
+    [TestCase("abbceffg", false)]
+    [TestCase("abbcegjk", false)]
+    [TestCase("abcdffaa", true)]
+    [TestCase("hepxxxyz", false)]
+    public void TestIsValid(string password, bool expectedResult)
+    {
+        Solution2015day0011.IsValid(password.ToArray()).Should().Be(expectedResult);
+    }
+    
     private const string MainInput = @"{input}";
 }
--- adventofcode/adventofcode.com/2015/Solution2015day0011.cs
+++ adventofcode/adventofcode.com/2015/Solution2015day0011.cs
@@ -1,6 +1,38 @@
 
+using System.Security.Cryptography;
+
 namespace adventofcode.adventofcode.com._2015;
 
-public class Solution2015day0011
+public static class Solution2015day0011
 {
+    public static char[] SolvePart1(char[] currentPassword)
+    {
+        do
+        {
+            Increment(currentPassword);
+        } while (!IsValid(currentPassword));
+
+        return currentPassword;
+    }
+
+    public static bool IsValid(char[] currentPassword)
+        => !currentPassword.Any(c => c is 'i' or 'o' or 'l')
+           && currentPassword.Zip<char, char, string>(currentPassword.Skip(1), (a, b) => a + "" + b)
+               .Zip<string, char, string>(currentPassword.Skip(2), (a, b) => a + b)
+               .Modify(Console.WriteLine)
+               .Any(seq => seq[0] == seq[1] - 1 && seq[1] == seq[2] - 1)
+           && currentPassword.Zip(currentPassword.Skip(1), (a, b) => (a == b)).ToList()
+               .And(list => list.Where((e, idx) => idx % 2 == 0 && e).Count() > 1
+                         || list.Where((e, idx) => idx % 2 != 0 && e).Count() > 1);
+           
+    private static void Increment(char[] currentPassword)
+    {
+        for (var i = 7; i >= 0; i--)
+        {
+            currentPassword[i]++;
+            if (currentPassword[i] <= 122)
+                break;
+            currentPassword[i] = (char)97;
+        }
+    }
 }
