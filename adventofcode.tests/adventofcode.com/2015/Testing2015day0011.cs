716c5f46da8fb5e8ffc917f7a4b0e6994e7ce28a
Cristian Bidea <cristian.bidea@uipath.com>
2023-08-23 23:10:36 +0300

    Solved day 11 but not very functional looking

--- adventofcode.tests/adventofcode.com/2015/Testing2015day0011.cs
+++ adventofcode.tests/adventofcode.com/2015/Testing2015day0011.cs
@@ -5,7 +5,10 @@ namespace adventofcode.tests.adventofcode.com._2015;
 
 public class Testing2015day0011
 {
-    [TestCase("hepxcrrq", "heqqaabc")]
+    [TestCase("hepxcrrq", "hepxxyzz")]
+    [TestCase("abcdefgh", "abcdffaa")]
+    [TestCase("ghijklmn", "ghjaabcc")]
+    [TestCase("hepxxyzz", "heqaabcc")]
     public void TestSolutionPart1(string password, string expectedNextPassword)
     {
         var nextPassword = Solution2015day0011.SolvePart1(password.ToArray());
@@ -17,6 +20,8 @@ public class Testing2015day0011
     [TestCase("abbcegjk", false)]
     [TestCase("abcdffaa", true)]
     [TestCase("hepxxxyz", false)]
+    [TestCase("ghjaabcc", true)]
+    [TestCase("abcdffaa", true)]
     public void TestIsValid(string password, bool expectedResult)
     {
         Solution2015day0011.IsValid(password.ToArray()).Should().Be(expectedResult);
--- adventofcode/adventofcode.com/2015/Solution2015day0011.cs
+++ adventofcode/adventofcode.com/2015/Solution2015day0011.cs
@@ -19,12 +19,21 @@ public static class Solution2015day0011
         => !currentPassword.Any(c => c is 'i' or 'o' or 'l')
            && currentPassword.Zip<char, char, string>(currentPassword.Skip(1), (a, b) => a + "" + b)
                .Zip<string, char, string>(currentPassword.Skip(2), (a, b) => a + b)
-               .Modify(Console.WriteLine)
                .Any(seq => seq[0] == seq[1] - 1 && seq[1] == seq[2] - 1)
-           && currentPassword.Zip(currentPassword.Skip(1), (a, b) => (a == b)).ToList()
-               .And(list => list.Where((e, idx) => idx % 2 == 0 && e).Count() > 1
-                         || list.Where((e, idx) => idx % 2 != 0 && e).Count() > 1);
-           
+           && HasTwoNonOverlappingPairs(currentPassword);
+
+    private static bool HasTwoNonOverlappingPairs(char[] currentPassword)
+    {
+        var list = new List<int>();
+        for (var i = 1; i < currentPassword.Length; i++)
+        {
+            if (currentPassword[i] == currentPassword[i - 1] && !list.Contains(i - 1))
+                list.Add(i);
+        }
+
+        return list.Count > 1;
+    }
+
     private static void Increment(char[] currentPassword)
     {
         for (var i = 7; i >= 0; i--)
