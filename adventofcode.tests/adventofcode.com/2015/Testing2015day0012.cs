5e9cec6c44f0f236dd154aa1442f18d0f4968c98
Cristian Bidea <cristian.bidea@uipath.com>
2023-08-26 20:53:59 +0300

    Solve day 12 (#6)
     Solve day 12

---
+++ adventofcode.tests/adventofcode.com/2015/Testing2015day0012.cs
@@ -0,0 +1,33 @@
+
+using System.Dynamic;
+using System.Net;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using adventofcode.adventofcode.com._2015;
+
+namespace adventofcode.tests.adventofcode.com._2015;
+
+public class Testing2015day0012
+{
+    [TestCase]
+    public void TestSolutionPart1()
+    {
+        var input = LoadInput();
+        Solution2015day0012.SolvePart1(input).Should().Be(111754);
+    }
+
+    [TestCase]
+    public void TestSolutionPart2()
+    {
+        var input = LoadInput();
+        Solution2015day0012.SolvePart2(input).Should().Be(65402);
+    }
+
+    private static string LoadInput()
+    {
+        var input = File.ReadAllText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!,
+            "adventofcode.com/2015/Assets/Testing2015day0012Input.txt"));
+        return input;
+    }
+}
---
+++ adventofcode/adventofcode.com/2015/Solution2015day0012.cs
@@ -0,0 +1,52 @@
+
+using System.Dynamic;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace adventofcode.adventofcode.com._2015;
+
+public partial class Solution2015day0012
+{
+    public class Environment
+    {
+        public List<JsonElement> List { get; set; } = new List<JsonElement>();
+        public int Sum { get; set; }
+    }
+    
+    public static int SolvePart1(string input)
+        => MatchNumberRegex().Matches(input)
+            .Select(m => int.Parse(m.Value))
+            .Aggregate((a, b) => a + b);
+
+    public static int SolvePart2(string input)
+        => JsonSerializer.Deserialize<ExpandoObject>(input)
+            .And(json => json
+                .Select(c => c.Value as JsonElement?)
+                .Where(obj => obj.HasValue)
+                .Select(obj => obj.Value)
+                .ToList())
+            .And(list => new Environment() { List = list })
+            .And(env => Enumerable.Range(0, Int32.MaxValue)
+                .TakeWhile(idx =>
+                {
+                    var node = env.List[idx];
+                    env.Sum += node.ValueKind == JsonValueKind.Number ? node.GetInt32() : 0;
+                    env.List.AddRange(node.ValueKind == JsonValueKind.Array 
+                        ? node.EnumerateArray().AsEnumerable() 
+                        : Array.Empty<JsonElement>().ToList());
+                    env.List.AddRange(node.ValueKind == JsonValueKind.Object &&
+                                      !node
+                                          .EnumerateObject()
+                                          .AsEnumerable()
+                                          .Any(c => c.Value.ValueKind == JsonValueKind.String &&
+                                                    c.Value.GetString() == "red")
+                        ? node.EnumerateObject().AsEnumerable().Select(p => p.Value)
+                        : Array.Empty<JsonElement>().ToList());
+                    return idx < env.List.Count - 1;
+                })
+                .Aggregate((a, b) => b)
+                .And(_ => env.Sum));
+    
+    [GeneratedRegex("-?[0-9]+")]
+    private static partial Regex MatchNumberRegex();
+}
