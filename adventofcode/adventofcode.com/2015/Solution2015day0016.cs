9141bea57f8f6d25527f8ce30617f0b79dba206f
Cristian Bidea <cristian.bidea@uipath.com>
2023-08-29 20:11:37 +0300

    Solve day 16

--- adventofcode/adventofcode.com/2015/Solution2015day0016.cs
+++ adventofcode/adventofcode.com/2015/Solution2015day0016.cs
@@ -1,6 +1,55 @@
 
+using System.Text.RegularExpressions;
+
 namespace adventofcode.adventofcode.com._2015;
 
-public class Solution2015day0016
+public static class Solution2015day0016
 {
+    public record Feature(string Name, int Value);
+
+    public record Sue(int Id, List<Feature> Features);
+
+    public static int SolvePart1(string input)
+        => ParseInput(input)
+            .And(list => list.Select(e => new
+                {
+                    Sue = e, Score =
+                        (e.Features.Any(f => f is { Name: "children", Value: 3 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "cats", Value: 7 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "samoyeds", Value: 2 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "pomeranians", Value: 3 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "akitas", Value: 0 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "vizslas", Value: 0 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "goldfish", Value: 5 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "trees", Value: 3 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "cars", Value: 2 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "perfumes", Value: 1 }) ? 1 : 0)
+                })
+                .MaxBy(s => s.Score)!.Sue.Id);
+
+    public static int SolvePart2(string input)
+        => ParseInput(input)
+            .And(list => list.Select(e => new
+                {
+                    Sue = e, Score =
+                        (e.Features.Any(f => f is { Name: "children", Value: 3 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "cats", Value: > 7 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "samoyeds", Value: 2 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "pomeranians", Value: < 3 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "akitas", Value: 0 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "vizslas", Value: 0 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "goldfish", Value: < 5 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "trees", Value: > 3 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "cars", Value: 2 }) ? 1 : 0) +
+                        (e.Features.Any(f => f is { Name: "perfumes", Value: 1 }) ? 1 : 0)
+                })
+                .MaxBy(s => s.Score)!.Sue.Id);
+
+    private static List<Sue> ParseInput(string input)
+        => input.Split('\n').Select(l => l.Trim())
+            .Select(l => Regex.Match(l, @"Sue (?<id>[0-9]+):(( ?(?<feature>[a-z]+): (?<feature_value>[0-9]+),?)\2*)+")
+                .And(match => match.Groups["feature"].Captures.Zip(match.Groups["feature_value"].Captures,
+                    (a, b) => new Feature(a.Value, b.Value.ToInt())).ToList()
+                    .And(features => new Sue(match.Groups["id"].Value.ToInt(), features))))
+            .ToList();
 }
