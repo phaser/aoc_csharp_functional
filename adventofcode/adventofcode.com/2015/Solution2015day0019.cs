3db69da9246e5b5e2b61f03e945ad0f96a0d0ced
Cristian Bidea <cristian.bidea@uipath.com>
2023-09-01 09:40:40 +0300

    Save status every step

--- adventofcode/adventofcode.com/2015/Solution2015day0019.cs
+++ adventofcode/adventofcode.com/2015/Solution2015day0019.cs
@@ -1,12 +1,15 @@
 
 using System.Collections.Immutable;
 using System.Formats.Asn1;
+using System.Runtime.InteropServices.JavaScript;
 using System.Text.RegularExpressions;
 
 namespace adventofcode.adventofcode.com._2015;
 
 public static partial class Solution2015day0019
 {
+    private static HashSet<string> _alreadySeen = new HashSet<string>();
+    
     public static long SolvePart1(string input)
         => input.Parse()
             .And(data => 
@@ -31,18 +34,23 @@ public static partial class Solution2015day0019
                     .Where(r => r.Key == "e")
                     .Select(r => r.Value)
                     .ToList();
-                var steps = 1;
+                long steps = 1;
                 while (true)
                 {
                     var newMolecules = currentMolecules
+                        .Where(molecule => !_alreadySeen.Contains(molecule))
                         .SelectMany(str => data.Rules
                             .SelectMany(rule => ExpandRule(str, rule)))
                         .Where(molecule => molecule.Length <= data.MedicineMolecule.Length)
+                        .Distinct()
+                        .AsParallel()
                         .ToList();
+                    _alreadySeen.UnionWith(currentMolecules);
+                    File.WriteAllText($"test_status_{steps}.txt",newMolecules.Aggregate((a, b) => a + Environment.NewLine + b));
                     steps++;
                     if (newMolecules.Any(molecule => molecule == data.MedicineMolecule))
                         break;
-                    currentMolecules = newMolecules;
+                    currentMolecules = newMolecules.Where(molecule => molecule.Length < data.MedicineMolecule.Length).ToList();
                 }
                 return steps;
             });
