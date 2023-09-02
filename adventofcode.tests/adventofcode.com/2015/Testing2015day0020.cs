697f870288ac13e0c800e344d91842a09fead2c1
Cristian Bidea <cristian.bidea@uipath.com>
2023-09-02 19:56:56 +0300

    Solve day 20

--- adventofcode.tests/adventofcode.com/2015/Testing2015day0020.cs
+++ adventofcode.tests/adventofcode.com/2015/Testing2015day0020.cs
@@ -6,14 +6,26 @@ namespace adventofcode.tests.adventofcode.com._2015;
 public class Testing2015day0020
 {
     [TestCase]
-    public void TestSolution()
+    public void TestSolutionPart1()
     {
-        Solution2015day0020.SolvePart1(33100000).Should().Be(776160);
+        Solution2015day0020.Solve(33100000, 10, 33100000).Should().Be(776160);
+    }
+
+    [TestCase]
+    public void TestSolutionPart2()
+    {
+        Solution2015day0020.Solve(33100000, 11, 50).Should().Be(786240);
     }
 
     [TestCase]
     public void TestSolutionPart1Iterative()
     {
-        Solution2015day0020.SolvePart1Iterative(33100000).Should().Be(776160);
+        Solution2015day0020.SolveIterative(33100000, 10, 33100000).Should().Be(776160);
+    }
+
+    [TestCase]
+    public void TestSolutionPart2Iterative()
+    {
+        Solution2015day0020.SolveIterative(33100000, 11, 50).Should().Be(786240);
     }
 }
--- adventofcode/adventofcode.com/2015/Solution2015day0020.cs
+++ adventofcode/adventofcode.com/2015/Solution2015day0020.cs
@@ -6,20 +6,22 @@ namespace adventofcode.adventofcode.com._2015;
 public static class Solution2015day0020
 {
     // This takes 25 seconds
-    public static int SolvePart1(int limit)
+    public static int Solve(int limit, int numberOfDeliveredPresentsPerHouse, int numberOfStops)
         => Enumerable.Range(1, limit / 10)
-            .SelectMany(elfIdx => Enumerable.Range(1, (limit / 10 + 1) / elfIdx)
-                .Select(idx => (HouseIndex: idx * elfIdx, Presents: elfIdx * 10)))
+            .SelectMany(elfIdx => Enumerable.Range(1, Math.Min((limit / 10) / elfIdx, numberOfStops))
+                .Select(idx => (HouseIndex: idx * elfIdx, Presents: elfIdx * numberOfDeliveredPresentsPerHouse)))
             .GroupBy(h => h.HouseIndex)
             .Select(group => (HouseIndex: group.Key, Sum: group.Select(g => g.Presents).Sum()))
             .First(house => house.Sum >= limit).HouseIndex;
 
     // This takes 273 ms
-    public static int SolvePart1Iterative(int limit)
+    public static int SolveIterative(int limit, int numberOfDeliveredPresentsPerHouse, int numberOfStops)
     {
         var houses = new int[limit / 10];
         for (var i = 1; i < houses.Length; i++)
-            for (var j = i; j < houses.Length; houses[j] += i * 10, j += i) { }
+        for (var j = i; j < houses.Length; houses[j] += i * numberOfDeliveredPresentsPerHouse, j += i)
+            if (j / i >= numberOfStops)
+                break;
 
         for (var i = 1; i < houses.Length; i++)
                 if (houses[i] >= limit)
