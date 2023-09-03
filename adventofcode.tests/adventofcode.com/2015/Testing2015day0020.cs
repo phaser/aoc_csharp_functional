ef8ac1ed174b145ee07f7946db5b6afe4a1c68c1
Cristian Bidea <cristian.bidea@uipath.com>
2023-09-03 21:50:24 +0300

    Improve on functional solution for day 20

--- adventofcode.tests/adventofcode.com/2015/Testing2015day0020.cs
+++ adventofcode.tests/adventofcode.com/2015/Testing2015day0020.cs
@@ -10,7 +10,7 @@ public class Testing2015day0020
     {
         Solution2015day0020.Solve(33100000, 10, 33100000).Should().Be(776160);
     }
-
+    
     [TestCase]
     public void TestSolutionPart2()
     {
--- adventofcode/adventofcode.com/2015/Solution2015day0020.cs
+++ adventofcode/adventofcode.com/2015/Solution2015day0020.cs
@@ -1,18 +1,52 @@
 
+using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 
 namespace adventofcode.adventofcode.com._2015;
 
+public class CompareELfs : IComparer<(int ElfId, int House)>
+{
+    public int Compare((int ElfId, int House) x, (int ElfId, int House) y)
+    {
+        return Comparer<int>.Default.Compare(x.Item2, y.Item2);
+    }
+}
+
 public static class Solution2015day0020
 {
     // This takes 25 seconds
-    public static int Solve(int limit, int numberOfDeliveredPresentsPerHouse, int numberOfStops)
+    public static int SolveB(int limit, int numberOfDeliveredPresentsPerHouse, int numberOfStops)
         => Enumerable.Range(1, limit / 10)
-            .SelectMany(elfIdx => Enumerable.Range(1, Math.Min((limit / 10) / elfIdx, numberOfStops))
-                .Select(idx => (HouseIndex: idx * elfIdx, Presents: elfIdx * numberOfDeliveredPresentsPerHouse)))
+            .SelectMany(elfIdx => 
+                Enumerable
+                    .Range(1, Math.Min((limit / 10) / elfIdx, numberOfStops))
+                    .Select(idx => (
+                        HouseIndex: idx * elfIdx,
+                        Presents: elfIdx * numberOfDeliveredPresentsPerHouse)))
             .GroupBy(h => h.HouseIndex)
             .Select(group => (HouseIndex: group.Key, Sum: group.Select(g => g.Presents).Sum()))
             .First(house => house.Sum >= limit).HouseIndex;
+
+    // this takes 2 seconds 
+    // just part 1 - I just validated that I can make it faster and keep it functional
+    public static int Solve(int limit, int numberOfDeliveredPresentsPerHouse, int numberOfStops)
+        => CreateDictionaryOfHouses(limit)
+            .And(currentElvesHouses => GetFirstHouseOverTheLimit(limit, currentElvesHouses));
+
+    private static int GetFirstHouseOverTheLimit(int limit, ReadOnlyDictionary<int, List<int>> currentElvesHouses)
+        => Enumerable.Range(1, limit / 10)
+            .First(house => (currentElvesHouses[house]
+                    .Select(elf => 
+                    { 
+                        currentElvesHouses[house + elf].Add(elf); 
+                        return elf; 
+                    })
+                    .Tap(_ => currentElvesHouses[2 * house].Add(house))
+                    .Select(elf => elf * 10).Sum() + house * 10)
+                .Tap(_ => currentElvesHouses[house].Clear()) >= limit);
+
+    private static ReadOnlyDictionary<int, List<int>> CreateDictionaryOfHouses(int limit)
+        => new(new Dictionary<int, List<int>>(Enumerable.Range(1, limit / 10).Select(house => new KeyValuePair<int, List<int>>(house, new List<int>()))));
 
     // This takes 273 ms
     public static int SolveIterative(int limit, int numberOfDeliveredPresentsPerHouse, int numberOfStops)
@@ -24,8 +58,9 @@ public static class Solution2015day0020
                 break;
 
         for (var i = 1; i < houses.Length; i++)
-                if (houses[i] >= limit)
-                    return i;
+            if (houses[i] >= limit)
+                return i;
+        
         return 0;
     }
 }
