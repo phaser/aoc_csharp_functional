91e6b1f23f768f1c5921f909b653dc753e99d5e6
Cristian Bidea <cristian.bidea@uipath.com>
2023-09-05 14:51:10 +0300

    Solve day 21

--- adventofcode.tests/adventofcode.com/2015/Testing2015day0021.cs
+++ adventofcode.tests/adventofcode.com/2015/Testing2015day0021.cs
@@ -1,11 +1,28 @@
 
+using adventofcode.adventofcode.com._2015;
+
 namespace adventofcode.tests.adventofcode.com._2015;
 
 public class Testing2015day0021
 {
     [TestCase]
-    public void TestSolution()
+    public void TestSolutionPart1()
     {
+        Solution2015day0021.SolvePart1(new Solution2015day0021.Player(103, 9, 2)).Should().Be(121);
+    }
+    
+    [TestCase]
+    public void TestSolutionPart2()
+    {
+        Solution2015day0021.SolvePart2(new Solution2015day0021.Player(103, 9, 2)).Should().Be(201);
+    }
+
+    [TestCase]
+    public void TestWhoWins()
+    {
+        var winner = Solution2015day0021.WhoWins(new Solution2015day0021.Player(8, 5, 5),
+            new Solution2015day0021.Player(12, 7, 2));
+        winner.Should().Be(0);
     }
 
     private const string MainInput = @"{input}";
