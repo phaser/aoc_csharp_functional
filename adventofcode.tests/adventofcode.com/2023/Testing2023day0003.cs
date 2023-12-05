2ac1de544acb7aac7e51e971393c150e6c8b6416
Cristian Bidea <cristian.bidea@uipath.com>
2023-12-05 16:53:37 +0200

    start solution

---
+++ adventofcode.tests/adventofcode.com/2023/Testing2023day0003.cs
@@ -0,0 +1,12 @@
+
+namespace adventofcode.tests.adventofcode.com._2023;
+
+public class Testing2023day0003
+{
+    [TestCase]
+    public void TestSolution()
+    {
+    }
+
+    private const string MainInput = @"{input}";
+}
---
+++ adventofcode/adventofcode.com/2023/Solution2023day0003.cs
@@ -0,0 +1,6 @@
+
+namespace adventofcode.adventofcode.com._2023;
+
+public class Solution2023day0003
+{
+}
