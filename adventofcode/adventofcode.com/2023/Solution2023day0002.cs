98470f960b4bb7971381b193f490340a8debc7ea
Cristian Bidea <cristian.bidea@uipath.com>
2023-12-05 14:19:08 +0200

    Decode games

--- adventofcode/adventofcode.com/2023/Solution2023day0002.cs
+++ adventofcode/adventofcode.com/2023/Solution2023day0002.cs
@@ -5,13 +5,40 @@ namespace adventofcode.adventofcode.com._2023;
 
 public class Solution2023day0002
 {
-    internal record Draw(int red, int green, int blue);
+    private record Draw(int red, int green, int blue);
 
-    internal record Game(int Id)
+    private record Game(int Id)
     {
-        public List<Draw> Draws = new List<Draw>();
+        public readonly List<Draw> Draws = new List<Draw>();
     }
 
     public static long SolvePart1(string input)
-        => throw new NotImplementedException();
+        => DecodeInput(input)
+            .ToArray()
+            .Map(_ => 0);
+
+    private static IEnumerable<Game> DecodeInput(string input)
+        => input.Split("\n")
+            .Select(line => line.Split(":")
+                .Map(gameArr =>
+                    new Tuple<Game, string[]>(new Game(int.Parse(gameArr[0].Replace("Game ", ""))), gameArr))
+                .Map(game => game.Item2[1].Split(";")
+                    .Select(drawStr => DecodeDraw(drawStr, game))
+                    .Map(draws =>
+                    {
+                        game.Item1.Draws.AddRange(draws);
+                        return game.Item1;
+                    })));
+
+    private static Draw DecodeDraw(string drawStr, Tuple<Game, string[]> game)
+        => drawStr.Split(",")
+            .Select(ComponentToDraw)
+            .Aggregate((a, b) => new Draw(a.red + b.red, a.green + b.green, a.blue + b.blue));
+
+    private static Draw ComponentToDraw(string component) 
+        => component.Contains("red")
+            ? new Draw(int.Parse(component.Replace("red", "")), 0, 0)
+            : component.Contains("green")
+                ? new Draw(0, int.Parse(component.Replace("green", "")), 0)
+                : new Draw(0, 0, int.Parse(component.Replace("blue", "")));
 }
