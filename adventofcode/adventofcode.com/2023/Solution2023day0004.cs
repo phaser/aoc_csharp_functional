07193f81418b10999af341bcca0def2d2d03fe98
Cristian Bidea <cristian.bidea@uipath.com>
2023-12-08 20:10:08 +0200

    Solve 2023 day 4 (#20)
    * Solve day 4 part 1
    * Solve day 4

---
+++ adventofcode/adventofcode.com/2023/Solution2023day0004.cs
@@ -0,0 +1,54 @@
+
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace adventofcode.adventofcode.com._2023;
+
+public partial class Solution2023day0004
+{
+    private record Card(int Id, IList<long> WinningNumbers, IList<long> CardNumbers);
+
+    private record ProblemData(Dictionary<int, int> Multipliers, IList<Card> Cards);
+    
+    public static long SolvePart1(string input)
+        => input.Split("\n")
+            .Select(line => ParseCard(line)
+                .Map(card => card.CardNumbers.Where(card.WinningNumbers.Contains))
+                .Map(winningNumbers => winningNumbers.Any() ? 1 << (winningNumbers.Count() - 1) : 0)
+            )
+            .Sum();
+
+    public static long SolvePart2(string input)
+        => input.Split("\n")
+            .Select(ParseCard)
+            .ToList()
+            .Map(cards => new ProblemData(new Dictionary<int, int>(cards.Select(card => new KeyValuePair<int, int>(card.Id, 1))), cards)
+            .Map(problemData => problemData.Cards
+                .Select(card => card.CardNumbers.Where(card.WinningNumbers.Contains)
+                    .ToList()
+                    .Map(winningNumbers =>
+                    {
+                        for (var i = 0; i < winningNumbers.Count; i++)
+                        {
+                            if (problemData.Multipliers.ContainsKey(card.Id + i + 1))
+                                problemData.Multipliers[card.Id + i + 1] += problemData.Multipliers[card.Id];
+                        }
+                        return problemData.Multipliers[card.Id]; 
+                    })
+                ))
+            )
+            .Sum();
+
+    private static Card ParseCard(string line)
+        => line.Split(":")
+            .Map(parts => parts[1]
+                .Split("|")
+                .Map(numbers => new Card(
+                    Id: int.Parse(parts[0].Replace("Card ", "")),
+                    WinningNumbers: NumberRegex().Matches(numbers[0]).Select(match => long.Parse(match.Value)).ToList(), 
+                    CardNumbers: NumberRegex().Matches(numbers[1]).Select(match => long.Parse(match.Value)).ToList()
+                    )));
+
+    [GeneratedRegex("[1-9]+[0-9]*")]
+    private static partial Regex NumberRegex();
+}
